6e30ba0ef399a1b27aeaf0a338f5885188d394d2
Paul Calabrese <pacalabre@gmail.com>
2026-01-20 15:15:49 -0700

    Added a link for each state

--- app/states/page.tsx
+++ app/states/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Link from "next/link";
 import { getStates, State } from "@/services/states";
 
 export default function StatesPage() {
@@ -24,13 +25,21 @@ export default function StatesPage() {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <main>
-      <h1>States</h1>
-      <ul>
+    <main className="mt-12">
+      <h1 className="text-accent text-3xl py-5 uppercase z-2 text-[35px] md:text-[50px] font-black [text-shadow:_-4px_6px_4px_rgba(0,0,0,0.25)]">
+        States
+      </h1>
+      <section className="flex flex-col gap-5 text-[30px] font-bold">
         {states.map((state) => (
-          <li key={state.id}>{state.name}</li>
+          <Link
+            key={state.id}
+            href={`/states/${state.id}`}
+            className="text-accent"
+          >
+            {state.name}
+          </Link>
         ))}
-      </ul>
+      </section>
     </main>
   );
 }
