ac97b2be34514f2f9e0a6910506b961728c9febc
Paul Calabrese <pacalabre@gmail.com>
2026-01-21 17:37:59 -0700

    Added state links in a container and styled it.

--- app/states/page.tsx
+++ app/states/page.tsx
@@ -25,11 +25,11 @@ export default function StatesPage() {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <main className="mt-12">
+    <main className="mt-12 flex justify-center flex-col items-center max-w-[700px] m-auto backdrop-blur-[7px] border-solid border-bg-ring border-[1px]">
       <h1 className="text-accent text-3xl py-5 uppercase z-2 text-[35px] md:text-[50px] font-black [text-shadow:_-4px_6px_4px_rgba(0,0,0,0.25)]">
         States
       </h1>
-      <section className="flex flex-col gap-5 text-[30px] font-bold">
+      <section className="flex flex-col gap-5 text-[30px] font-bold my-[20px]">
         {states.map((state) => (
           <Link
             key={state.id}
