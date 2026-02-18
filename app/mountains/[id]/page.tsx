8ad452a4914c12b381e9e0ce02c1b6683c59393f
Paul Calabrese <pacalabre@gmail.com>
2026-02-18 12:38:21 -0700

    Added a some template code to the individual mountain page

--- app/mountains/[id]/page.tsx
+++ app/mountains/[id]/page.tsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import { getMountain } from "@/services/mountains";
 import { Mountain } from "@/app/types/mountainTypes";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function MountainPage() {
   const params = useParams();
@@ -27,9 +29,39 @@ export default function MountainPage() {
   if (error) return <div>Error: {error}</div>;
   if (!mountain) return <div>Mountain not found</div>;
 
+  const exampleAddress = "123 Mountain Road, Ski Town, CO 80435";
+  const imageUrl = `/mountain-logos/${mountain.name}.png`;
+
+  const handleDirectionsClick = () => {
+    const encodedAddress = encodeURIComponent(exampleAddress);
+    window.open(`maps://maps.apple.com/?daddr=${encodedAddress}`, "_blank");
+  };
+
   return (
-    <main>
-      <h1>{mountain.name}</h1>
+    <main className="mt-12 flex justify-center flex-col items-center max-w-[700px] m-auto backdrop-blur-[15px] border-solid border-bg-ring border-[1px] p-[20px]">
+      {imageUrl && (
+        <Image src={imageUrl} alt="image" width={200} height={200} />
+      )}
+
+      <section>
+        <p>{exampleAddress}</p>
+        <Button onClick={handleDirectionsClick} className="max-w-[150px]">
+          Get Directions
+        </Button>
+      </section>
+      <section className="flex justify-center">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris.
+        </p>
+      </section>
+      <section>
+        <h2>Weather</h2>
+        <p>Current Conditions: Sunny, 28°F</p>
+        <p>Wind: 5 mph NW</p>
+        <p>Snow Base: 48 inches</p>
+      </section>
     </main>
   );
 }
