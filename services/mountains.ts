e96f0340087350ff09dcb818e43bb0cb8baf48a2
Paul Calabrese <pacalabre@gmail.com>
2026-02-19 17:23:02 -0700

    Added services for getting resort stats and weather

--- services/mountains.ts
+++ services/mountains.ts
@@ -21,3 +21,23 @@ export async function getMountain(id: string): Promise<Mountain> {
 
   return response.json();
 }
+
+export async function getMountainWeather(lat: number, lon: number) {
+  const weatherUrl = `${process.env.NEXT_PUBLIC_WEATHER_URL}/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`;
+
+  const response = await fetch(weatherUrl);
+  if (!response.ok) {
+    throw new Error("Failed to fetch weather");
+  }
+  return response.json();
+}
+
+export async function getResortStatus(mountainName: string) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_RESORT_STATUS_URL}/${mountainName}`,
+  );
+  if (!response) {
+    throw new Error("Failed to fecth resort status");
+  }
+  return response.json();
+}
